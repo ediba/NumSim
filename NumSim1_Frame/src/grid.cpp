4ca868d2078f752faaf1bfd89088c2aa23d493d4
ezugor1 <ezugor1@etf.unsa.ba>
2016-11-07 14:55:55 +0100

    added DC functions in Grid

--- NumSim1_Frame/src/grid.cpp
+++ NumSim1_Frame/src/grid.cpp
@@ -37,22 +37,43 @@ const real_t &Grid::Cell(const Iterator &it) const{
 
 
 real_t Grid::dx_l(const Iterator& it) const
-{return (_data[it.Value()] - _data[it.Left().Value()])/((_geom->Mesh())[0]);}
+{return (_data[it] - _data[it.Left()])/((_geom->Mesh())[0]);}
 
 real_t Grid::dx_r(const Iterator& it) const
-{return (_data[it.Right().Value()] - _data[it.Value()])/((_geom->Mesh())[0]);}
+{return (_data[it.Right()] - _data[it])/((_geom->Mesh())[0]);}
 
 real_t Grid::dy_l(const Iterator& it) const
-{return (_data[it.Value()] - _data[it.Down().Value()])/((_geom->Mesh())[1]);}
+{return (_data[it] - _data[it.Down()])/((_geom->Mesh())[1]);}
 
 real_t Grid::dy_r(const Iterator& it) const
-{return (_data[it.Top().Value()] - _data[it.Value()])/((_geom->Mesh())[1]);}
-///central difference
-real_t Grid::dx_c(const Iterator& it) const
-{
-    return ((_data[it.Right().Value()] - _data[it.Value()])/((_geom->Mesh())[0])+(_data[it.Value()] - _data[it.Left().Value()])/((_geom->Mesh())[0]))*0.5;
+{return (_data[it.Top()] - _data[it])/((_geom->Mesh())[1]);}
+///
+
+
+real_t Grid:: dxx(const Iterator &it) const{
+    return (_data[it.Right()]-2*_data[it]+_data[it.Left()])/(((_geom->Mesh())[0])*((_geom->Mesh())[1]));
+}
+/// Computes the central difference quatient of 2nd order in y-dim at [it]
+real_t Grid::dyy(const Iterator &it) const{
+    return (_data[it.Top()]-2*_data[it]+_data[it.Down()])/(((_geom->Mesh())[1])*((_geom->Mesh())[1]));
 }
 
+/// Computes u*du/dx with the donor cell method
+real_t Grid::DC_udu_x(const Iterator &it, const real_t &alpha) const{
+
+    real_t DC_udu_x_var, left_var,right_var;
+    left_var= (((_data[it]+_data[it.Right()])/2)*((_data[it]+_data[it.Right()])/2)-((_data[it]+_data[it.Left()])/2)*((_data[it]+_data[it.Left()])/2))/ ((_geom->Mesh())[0]);
+    right_var=alpha*(-std::fabs((_data[it]+_data[it.Right()])/2)*dx_r(it)/2+std::fabs((_data[it]+_data[it.Left()])/2)*dx_l(it)/2)/ ((_geom->Mesh())[0]);
+
+
+     return left_var+right_var;
+}
+/// Computes v*du/dy with the donor cell method
+real_t DC_vdu_y(const Iterator &it, const real_t &alpha, const Grid *v) const;
+/// Computes u*dv/dx with the donor cell method
+real_t DC_udv_x(const Iterator &it, const real_t &alpha, const Grid *u) const;
+/// Computes v*dv/dy with the donor cell method
+real_t DC_vdv_y(const Iterator &it, const real_t &alpha) const;
 
 ///implement all other derivatives
 
