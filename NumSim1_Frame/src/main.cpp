513cb9bba69310942e859e791974a2e02ab27d50
FelixWein <felix.weinhardt@gmx.de>
2016-11-11 16:33:56 +0100

    main als test file für boundary iterator

--- NumSim1_Frame/src/main.cpp
+++ NumSim1_Frame/src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv) {
   
   param.Load("default.param");
   geom.Load("default.geom");
-  Grid testgrid = Grid(&geom);
+  
   multi_index_t size = geom.Size();
   std::cout << "Size = "<< size[0] <<" " <<size[1] <<std::endl;
     multi_real_t length = geom.Length();
@@ -43,15 +43,16 @@ int main(int argc, char **argv) {
     multi_real_t mesh = geom.Mesh();
     std::cout << "Mesh = "<< mesh[0] << " " << mesh[1] << std::endl;
     
-    /*
-    
-    testgrid.Initialize(2);
-    
-    
-    multi_real_t InterX = {0.5, 1};
-    real_t TestWert = testgrid.Interpolate(InterX);
-    
-    std::cout << "Interpoliert an der Stelle "<< InterX[0] << "/"<< InterX[1]<< " = " <<TestWert<<std::endl;
+  
+    multi_real_t offset = {0.5, 0.5};
+    Grid testgrid = Grid(&geom, offset);
+//     testgrid.Initialize(2);
+//     InteriorIterator iter(&geom);
+//     testgrid.Cell(iter) = 0;
+//     multi_real_t InterX = {1.5, 1.5};
+//     real_t TestWert = testgrid.Interpolate(InterX);
+//     
+//     std::cout << "Interpoliert an der Stelle "<< InterX[0] << "/"<< InterX[1]<< " = " <<TestWert<<std::endl;
     
    
 //    const Grid *visugrid;
@@ -60,33 +61,61 @@ int main(int argc, char **argv) {
 //    visugrid = comp.GetVelocity();
 
   //visualize testgrid
-    Renderer renderer = Renderer(length, mesh);
-    renderer.Init(200,200,100);
-    renderer.Render(&testgrid);
-    renderer.ShowGrid(true);
-    std::cin.get();
-    
-    */
-    
-    /*//test iterator
+//     Renderer renderer = Renderer(length, mesh);
+//     renderer.Init(200,200,100);
+//     renderer.Render(&testgrid);
+//     renderer.ShowGrid(true);
+//     std::cin.get();
+//     
+// 
+//     
+    //test iterator
     Grid testgrid2 = Grid(&geom,{0.5,0.5});
     testgrid2.Initialize(1);
-    Iterator test2iterator(&geom,0);
+    //Iterator test2iterator(&geom,0);
+    //InteriorIterator test2iterator(&geom);
+    BoundaryIterator test2iterator(&geom);
+    test2iterator.SetBoundary(3);
+    test2iterator.First();
     testgrid2.Cell(test2iterator) = 0;
+    //testgrid2.Cell(test2iterator) = 1;
     Renderer renderer2 = Renderer(length, mesh);
-    renderer2.Init(400,400,0);
+    renderer2.Init(500,500,0);
     renderer2.Render(&testgrid2);
     renderer2.ShowGrid(true);
-    for(int i=0; i<30; i++){
+    real_t t;
+    while (test2iterator.Valid()){
         std::cout << "Iterator value: " << test2iterator.Value() << std::endl;
+        std::cout << "\t left = " <<  testgrid2.Cell(test2iterator.Left()) << " mid = "<<  testgrid2.Cell(test2iterator) << " right = " <<  testgrid2.Cell(test2iterator.Right()) << std::endl;
+        /*std::cout << "Unterste Zeile: ";
+        for(int j=0; j<5; j++){
+            std::cout << testgrid2.Interpolate({0.5+j,0.5}) << "  ";
+        }
+        std::cout << std::endl;*/
         std::cin.get();
         testgrid2.Cell(test2iterator) = 1;
         test2iterator.Next();
         testgrid2.Cell(test2iterator) = 0;
+        //t = testgrid2.Interpolate({0.0,0.5});
+        //std::cout << " interpoliert an der stelle 0.0 0.5 = " << t << std::cout; 
+
         renderer2.Render(&testgrid2);
-    }
-    */
         
+    }
+//     for(int i=0; i<50; i++){
+//         std::cout << "Iterator value: " << test2iterator.Value() << std::endl;
+//         /*std::cout << "Unterste Zeile: ";
+//         for(int j=0; j<5; j++){
+//             std::cout << testgrid2.Interpolate({0.5+j,0.5}) << "  ";
+//         }
+//         std::cout << std::endl;*/
+//         std::cin.get();
+//         testgrid2.Cell(test2iterator) = 1;
+//         test2iterator.Next();
+//         testgrid2.Cell(test2iterator) = 0;
+//         renderer2.Render(&testgrid2);
+//     }
+    
     // Run a few steps
 //     for (uint32_t i = 0; i < 9; ++i)
 //       comp.TimeStep(false);
